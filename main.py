0a1c11823e4310572817d7827c9785f7ff53f8e4
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2024-06-25 14:44:07 -0500

    counting words of book

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+    
+    count_words(file_contents)
 
+def count_words(text):
+    string_list = text.split()
 
+    print(len(string_list))
 
 
 
