5a3eb5756a16d3834d03d3e9bda1e8130f79a6a6
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2024-06-25 15:21:20 -0500

    printing a report of characters from book

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
     
-    count_words(file_contents)
+    print("--- End report ---")
 
-def count_words(text):
+
+def get_num_words(text):
     string_list = text.split()
-
     print(len(string_list))
 
+def sort_on(d):
+    return d["num"]
 
+def get_chars_dict(text):
+    characters_dict = {}
 
+    for i in text:
+        if i.lower() in characters_dict:
+            characters_dict[i.lower()] += 1
+        else:
+            characters_dict[i.lower()] = 1
 
+    return characters_dict
 
-
-
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num" : num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+            
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 if __name__ == "__main__":
     main()
